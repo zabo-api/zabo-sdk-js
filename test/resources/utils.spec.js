f00723b518bde9868918f5ce2cd9c6be8335b2a4
William Dias <wrddias@gmail.com>
2019-11-09 14:03:42 -0500

    Expose Zabo.utils.getBytecode() + tests

--- test/resources/utils.spec.js
+++ test/resources/utils.spec.js
@@ -4,9 +4,7 @@ const should = require('should')
 const sdk = require('../../src/index.js')
 
 describe('Zabo SDK Utils', () => {
-  let utils = null
-
-  it('should not be instantiated during zabo.init() running outside a browser', async function () {
+  it('should be instantiated during zabo.init()', async function () {
     await sdk.init({
       apiKey: 'some-api-key',
       secretKey: 'some-secret-key',
@@ -14,15 +12,62 @@ describe('Zabo SDK Utils', () => {
       autoConnect: false
     }).catch(err => err).should.be.ok()
 
-    utils = require('../../src/resources/utils')(sdk.api)
-
-    utils.should.have.property('getQRCode')
+    sdk.utils.should.have.property('getQRCode')
+    sdk.utils.should.have.property('getBytecode')
   })
 
-  it('utils.getQRCode() should return a QRCode image', async function () {
-    let qrCodeImage = utils.getQRCode('test')
+  it('utils.getQRCode() should return a QRCode image', function () {
+    let qrCodeImage = sdk.utils.getQRCode('test')
 
     qrCodeImage.should.containEql('<img src=')
+  })
+
+  it('utils.getBytecode() should fail if `fromAddress` is missing', async function () {
+    let response = await sdk.utils.getBytecode({
+      toAddress: '0x0',
+      amount: '0.0001',
+      currency: 'ETH'
+    }).should.be.rejected()
+
+    response.should.be.an.Error()
+    response.error_type.should.be.equal(400)
+    response.message.should.containEql('fromAddress')
+  })
+
+  it('utils.getBytecode() should fail if `toAddress` is missing', async function () {
+    let response = await sdk.utils.getBytecode({
+      fromAddress: '0x0',
+      amount: '0.0001',
+      currency: 'ETH'
+    }).should.be.rejected()
+
+    response.should.be.an.Error()
+    response.error_type.should.be.equal(400)
+    response.message.should.containEql('toAddress')
+  })
+
+  it('utils.getBytecode() should fail if `amount` is missing', async function () {
+    let response = await sdk.utils.getBytecode({
+      fromAddress: '0x0',
+      toAddress: '0x0',
+      currency: 'ETH'
+    }).should.be.rejected()
+
+    response.should.be.an.Error()
+    response.error_type.should.be.equal(400)
+    response.message.should.containEql('amount')
+  })
+
+  it('utils.getBytecode() should fail if `currency` is missing', async function () {
+    let response = await sdk.utils.getBytecode({
+      fromAddress: '0x0',
+      toAddress: '0x0',
+      amount: '0.0001'
+    }).should.be.rejected()
+
+    response.should.be.an.Error()
+    response.error_type.should.be.equal(400)
+    response.message.should.containEql('currency')
   })
 
 })
