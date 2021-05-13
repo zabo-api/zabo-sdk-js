92bc234074d303e4efb9aff29ca4e7356f6d7e1c
Zane Helton <zane99@me.com>
2021-05-13 10:09:29 -0600

    Ready for NPM?

--- @types/err.d.ts
+++
@@ -1,5 +0,0 @@
-export class SDKError extends Error {
-    constructor(type: any, msg: any, requestId: any, ...params: any[]);
-    error_type: any;
-    request_id: any;
-}
--- @types/resources/index.d.ts
+++
@@ -1,2 +0,0 @@
-declare function _exports(api: import('../api'), isNode: boolean): any;
-export = _exports;
